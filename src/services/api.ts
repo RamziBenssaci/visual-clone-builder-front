6791468bf8e549427420ad26075bc451eb5947c1
RamziBenssaci <129678228+RamziBenssaci@users.noreply.github.com>
2025-07-12 05:21:04 +0100

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,8 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'https://test.freedomprocessing3.com/api',
   headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
+     'Accept': 'application/json',
+
   },
 });
 
